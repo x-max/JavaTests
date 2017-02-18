7b03f95eaefa1572d299425cd948d9a678de93c3
806590 <nemanja.maksic@gmail.com>
2017-02-18 16:49:09 +0100

    test

---
+++ src/org/DDT/XMLUtil.java
@@ -0,0 +1,5 @@
+package org.DDT;
+
+public class XMLUtil {
+
+}
---
+++ src/org/pom/TestCaller.java
@@ -0,0 +1,17 @@
+package org.pom;
+
+import org.testng.annotations.Test;
+
+public class TestCaller {
+  
+	
+	@Test
+  public void Method1() {
+		System.out.println("TestMethod1");
+  }
+	
+	@Test
+  public void Method2(){
+		System.out.println("TestMethod2");
+	}
+}
